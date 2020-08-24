426ec408696b219d955acba9ea66e94e9866f7e7
Diogo Soares <diogosoliv5@gmail.com>
2020-08-23 23:54:50 -0300

    teste 1f

---
+++ src/estudoJavaCederj/teste.java
@@ -0,0 +1,14 @@
+package estudoJavaCederj;
+
+public class teste {
+
+	public teste() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
